ccd53550b7890f7fec671cd8cbf27e9753474d8c
antONget <a.l.ponomare@mail.ru>
2023-12-25 23:20:22 +0300

    admin menu

--- bot.py
+++ bot.py
@@ -3,15 +3,20 @@ from telegram_bot_calendar import DetailedTelegramCalendar, LSTEP
 import keyboards
 import main
 import googleSheets
+import os
+from pydub import AudioSegment
+import time
 
+# bot = telebot.TeleBot('6689173506:AAFjbmjGd124jwLQyS_XA1VVtCPLvhhgCfE') # test
 bot = telebot.TeleBot('6490398143:AAH33JclTur7nlX4dxwtycOFADRjY2x5FyM')
-user = {"volume": 0, "back": 'None'}
+user = {"flag_contact": False, "back": 'None', "phone": 'picture'}
 
 
 # обработка текстовых сообщений
 @bot.message_handler(content_types=['text'])
 def get_text_messages(message):
     if message.text == "/start":
+        googleSheets.append_name_start(message.chat.id)
         keyboard = keyboards.start_keyboard()
         bot.send_message(message.from_user.id,
                          text="Здесь вы сможете создать свою персональную аффирмацию за считанные минуты, рассчитать"
@@ -23,6 +28,42 @@ def get_text_messages(message):
                                                " считанные минуты, рассчитать и узнать как активировать ваш код"
                                                " богатства, а также заказать картину или записаться на офлайн"
                                                " мастер-класс. ")
+    elif message.text == "/admin":
+        if message.chat.id in [843554518]:
+            bot.send_message(chat_id=message.chat.id,
+                             text='Вы являетесь администратором и вам доступен расширенный функционал бота!')
+            # users = googleSheets.get_all_user()
+            users = [843554518, 463672403, 5443784834]
+            for i, u in enumerate(users):
+                time.sleep(1)
+                try:
+                    bot.send_message(chat_id=u,
+                                     text='На связи Мария Чечина, и я рада вам сообщить о долгожданном старте продаж'
+                                          ' нейрокартин «Акварели для цели». Вы получили это сообщение, потому что проявили'
+                                          ' интерес к моему уникальному продукту, не имеющему аналогов на рынке.\n\n'
+                                          'Приобретая картину, вы получаете целый комплекс практик, направленных для'
+                                          ' реализации ваших целей. Это самопрограммирование сознания с научнодоказанным'
+                                          ' фундаментом:\n'
+                                          '– Работа с телом\n'
+                                          '– Медитации\n'
+                                          '– Эзотерика с секретной техникой\n'
+                                          '– Визуальный стимул, который будет работать с вашим сознанием.\n'
+                                          'И это только часть того, что вы получаете!\n\n'
+                                          'Успейте стать первым обладателем картины Акварели для цели! Выбирайте сюжет,'
+                                          ' который понравился и рисуйте свой уникальный путь к личному успеху и реализации'
+                                          ' целей!\n\n'
+                                          'Количество картин ограничено!\n'
+                                          'Для заказа переходите в раздел «Нейрокартины» или по '
+                                          '<a href="https://www.wildberries.ru/catalog/195632159/detail.aspx?targetUrl=GP&size=317794367">ссылке.</a>',
+                                     parse_mode='HTML')
+                    bot.send_message(chat_id=843554518,
+                                     text=f'{i+1}/{len(users)} - Пользователь {u} оповещен!')
+                except:
+                    bot.send_message(chat_id=843554518,
+                                     text=f'{i+1} - Пользователь {u} не оповещен!')
+        else:
+            bot.send_message(chat_id=message.chat.id,
+                             text='Вы не являетесь администратором и вам не доступен расширенный функционал бота!')
     else:
         bot.send_message(message.from_user.id, "Я тебя не понимаю. Напиши /help.")
 
@@ -30,28 +71,42 @@ def get_text_messages(message):
 # обработка записонного голосового сообщения
 @bot.message_handler(content_types=['voice'])
 def voice_processing(message):
+    print("voice_processing")
+    # bot.clear_step_handler(message)
     file_info = bot.get_file(message.voice.file_id)
     downloaded_file = bot.download_file(file_info.file_path)
 
-    with open(f'media/{message.chat.id}new_file.ogg', 'wb') as new_file:
+    with open(f'data/{message.chat.id}new_file.ogg', 'wb') as new_file:
         new_file.write(downloaded_file)
-    main.convert_ogg_to_mp3(f'media/{message.chat.id}new_file.ogg', f'media/{message.chat.id}new_file.mp3')
-    main.affirmacia(user["volume"], message.chat.id, user["back"])
-    audio = open(rf'media/{message.chat.id}аффирмация.mp3', 'rb')
+    # link = f"<a href='https://t.me/AntonPon0marev'>технической поддержкой.</a>"
+    bot.send_message(chat_id=message.chat.id, text='Немного подождите, ваша аффирмация обрабатывается...⏳\n'
+                                                   'Если время ожидания составит более 1 мин, напишите в поддержку.')
+    # main.affirmacia(message.chat.id, user["back"])
+    main.affirmacia(message.chat.id, user["back"])
+    audio = open(f"data/{message.chat.id}aff.mp3", 'rb')
+
+    bot.send_message(843554518, text=f'аффирмация для {message.chat.id} создана')
     bot.send_audio(message.chat.id, audio)
+    # bot.send_audio(843554518, audio)
     audio.close()
 
 
 # обработка отправленного контакта
 @bot.message_handler(content_types=['contact'])
 def contact(message):
+    print("contact")
     if message.contact is not None:  # Если присланный объект <strong>contact</strong> не равен нулю
-        bot.register_next_step_handler(message, googleSheets.update_phone(message))
+        if user["phone"] == "picture":
+            bot.register_next_step_handler(message, googleSheets.update_phone(message))
+        else:
+            bot.register_next_step_handler(message, googleSheets.update_phone_master(message))
 
 
 # обработка нажатия кнопки КОД БОГАТСТВА
 @bot.callback_query_handler(func=lambda call: call.data == "codewealth")
 def callback_code(call):
+    print("callback_code")
+    bot.clear_step_handler(call.message)
     bot.send_message(call.message.chat.id,
                      text='Код на богатство (его также называют денежный или финансовый код) 💵- это пароль из цифр;'
                           ' комбинация чисел денежной удачи. Он бывает личный и универсальный. Универсальным '
@@ -69,6 +124,7 @@ def callback_code(call):
 # обработка выбора даты рождения
 @bot.callback_query_handler(func=DetailedTelegramCalendar.func())
 def cal(c):
+    print("cal")
     result, key, step = DetailedTelegramCalendar().process(c.data)
     if not result and key:
         bot.edit_message_text(f"Select {LSTEP[step]}",
@@ -80,64 +136,84 @@ def cal(c):
         bot.edit_message_text(f"Ваша дата рождения {result}, Ваш код богатства {code}",
                               c.message.chat.id,
                               c.message.message_id)
+        bot.send_message(c.message.chat.id,
+                         text="Активацию финансового кода можно произвести любым из способов:\n"
+                            "— написать число на картине для исполнения желаний;\n"
+                            " — написать число на купюре крупного номинала или найти с такими же цифрами в номере, и носить с собой;\n"
+                            "— записать код на бумаге и положить там, где хранятся сбережения;\n"
+                            "— добавить цифры кода в пароль на аккаунты, почту или поставить на банковскую карту, телефон и др.\n"
+                            "— окружить себя таким числовым сочетание: написать в ежедневнике или на другом видном тебе месте,"
+                              " выбрать номер телефона с этими цифрами, указывать их при заключении договоров и др.")
 
 
 # ПОЛУЧИТЬ АФФИРМАЦИЮ
 @bot.callback_query_handler(func=lambda call: call.data == "affermacia")
 def callback_affirmacia(call):
+    print("callback_affirmacia")
+    bot.clear_step_handler(call.message)
     keyboard = keyboards.affirmacia_keyboard()
     bot.send_message(call.message.chat.id,
-                     text='Запишите свою персональную аффирмацию и каждое утро начинаете с установки, которая '
-                          'направлена на реализацию вашего желания.'
-                        '\n'
-                        '\n'
-                        'Для записи аффирмации вам потребуется:\n'
-                        '— Сформировать ваше желание в настоящем времени, как будто оно уже осуществилось.'
-                        ' Желания должно касаться только вас.\n'
-                        '— Исключить в формулировках отрицание (частицу «не»), сконцентрироваться на положительном.\n'
-                        '— Поставить реалистичную дату реализации. Описать то, что вы чувствуете от реализации желания'
-                        'и описать это как можно детальней.'
-                        '\n'
-                        '\n'
-                        'Например: «Я купила 3-комнатную квартиру с панорамными окнами площадью 100 кв.м.'
-                        ' в городе Барнауле в прекрасном районе. Вместе с парковочным местом. Осенью 2024 года.'
-                        ' У меня дизайнерский ремонт, стильная просторная гардеробная, в моей спальне личная'
-                        ' просторная ванная комната. Я счастлива в этой квартире со своей семьей»',
+                     text='Запишите свою персональную аффирмацию и каждое утро начинаете с положительной установки.\n\n'
+                          ' — Аффирмацию сформулируйте  в настоящем времени, как будто она уже осуществилась.'
+                          ' Аффирмация  должна касаться только вас.\n'
+                          '— Исключите в формулировках отрицание (частицу «не»), сконцентрируйтесь на положительном.\n\n'
+                          'Например:\n'
+                          '«— Я излучаю радость, энергию , счастье, успех.\n'
+                          ' — Я денежный магнит! Деньги беспрерывно потоком текут ко мне из разных источников».\n\n'
+                          'Выберите фон для вашей аффирмации.',
                      reply_markup=keyboard)
 
 
 # выбор мелодии волна
 @bot.callback_query_handler(func=lambda call: call.data == "volna")
 def callback_audio(call):
+    print("callback_audio")
     bot.send_message(chat_id=call.message.chat.id, text='Идет загрузка выбранной мелодии...⏳')
     bot.send_audio(chat_id=call.message.chat.id, audio=open('media/волна.mp3', 'rb'))
+
     user["back"] = 'волна'
-    msg = bot.send_message(chat_id=call.message.chat.id, text='Укажите уровень громкости фона (от -20 до 20)')
-    bot.register_next_step_handler(msg, test_msg)
+    # msg = bot.send_message(chat_id=call.message.chat.id, text='Укажите уровень громкости фона (от -20 до 20)')
+    # bot.register_next_step_handler(msg, test_msg)
+    bot.send_message(chat_id=call.message.chat.id,
+                     text='Запишите и отправьте голосовое сообщение не более 1 мин. Во время записи будьте в хорошем'
+                          ' расположении духа, уединитесь, чтобы исключить посторонние'
+                          ' звуки. Круче всего получается под одеялом ☺️')
 
 
 # выбор мелодии гарминия
 @bot.callback_query_handler(func=lambda call: call.data == "garmonia")
 def callback_audio(call):
+    print("callback_audio")
     bot.send_message(chat_id=call.message.chat.id, text='Идет загрузка выбранной мелодии...⏳')
     bot.send_audio(chat_id=call.message.chat.id, audio=open('media/гармония.mp3', 'rb'))
+
     user["back"] = 'гармония'
-    msg = bot.send_message(chat_id=call.message.chat.id, text='Укажите уровень громкости фона (от -20 до 20)')
-    bot.register_next_step_handler(msg, test_msg)
+    # msg = bot.send_message(chat_id=call.message.chat.id, text='Укажите уровень громкости фона (от -20 до 20)')
+    # bot.register_next_step_handler(msg, test_msg)
+    bot.send_message(chat_id=call.message.chat.id,
+                     text='Запишите и отправьте голосовое сообщение не более 1 мин. Во время записи будьте в хорошем'
+                          ' расположении духа, уединитесь, чтобы исключить посторонние'
+                          ' звуки. Круче всего получается под одеялом ☺️')
 
 
 # выбор мелодии релах
 @bot.callback_query_handler(func=lambda call: call.data == "relax")
 def callback_audio(call):
+    print("callback_audio")
     bot.send_message(chat_id=call.message.chat.id, text='Идет загрузка выбранной мелодии...⏳')
     bot.send_audio(chat_id=call.message.chat.id, audio=open('media/релакс.mp3', 'rb'))
-    user["back"] = 'релакс'
-    msg = bot.send_message(chat_id=call.message.chat.id, text='Укажите уровень громкости фона (от -20 до 20)')
-    bot.register_next_step_handler(msg, test_msg)
 
+    user["back"] = 'релакс'
+    # msg = bot.send_message(chat_id=call.message.chat.id, text='Укажите уровень громкости фона (от -20 до 20)')
+    # bot.register_next_step_handler(msg, test_msg)
+    bot.send_message(chat_id=call.message.chat.id,
+                     text='Запишите и отправьте голосовое сообщение не более 1 мин. Во время записи будьте в хорошем'
+                          ' расположении духа, уединитесь, чтобы исключить посторонние'
+                          ' звуки. Круче всего получается под одеялом ☺️')
 
 # проверка на корректность введенного уровня горомкости мелодии
 def test_msg(msg):
+    print("test_msg")
     try:
         print(msg.json['text'])
         user["volume"] = int(msg.json['text'])
@@ -153,6 +229,8 @@ def test_msg(msg):
 # обработка нажатия клавиши НЕЕРОКАРТИНА
 @bot.callback_query_handler(func=lambda call: call.data == "picture")
 def callback_picture(call):
+    print("callback_picture")
+    bot.clear_step_handler(call.message)
     bot.send_message(call.message.chat.id,
                      text='Совсем скоро вы сможете заказать картины по номерам *Акварели для цели* '
                           'с популярного маркетплэйса Wildberries. Во время рисования, вы погружаетесь в гармоничное '
@@ -163,28 +241,57 @@ def callback_picture(call):
                           'Узнать о наличии первыми можно заполнив анкету предзаказа.',
                      parse_mode="Markdown")
     msg = bot.send_message(call.message.chat.id, text='Укажите ваши имя и фамилию')
+    user["flag_contact"] = True
     bot.register_next_step_handler(msg, get_contact)
 
 
 # получение и запись контакта введенного вручную
 def get_contact(message):
-    googleSheets.append_name(message.chat.id, message.text)
-    keyboard = keyboards.contact_keyboard()
-    msg = bot.send_message(message.chat.id, text='Укажите ваш номер телефона', reply_markup=keyboard)
-    bot.register_next_step_handler(msg, googleSheets.update_phone)
+    print("get_contact")
+    if user["flag_contact"]:
+        user["phone"] = "picture"
+        googleSheets.append_name(message.chat.id, message.text)
+        keyboard = keyboards.contact_keyboard()
+        msg = bot.send_message(message.chat.id, text='Укажите ваш номер телефона', reply_markup=keyboard)
+        bot.register_next_step_handler(msg, picture_finish)
+        user["flag_contact"] = False
 
+def get_contact_master(message):
+    print("get_contact_master")
+    if user["flag_contact"]:
+        user["phone"] = "master"
+        googleSheets.append_name_master(message.chat.id, message.text)
+        keyboard = keyboards.contact_keyboard()
+        msg = bot.send_message(message.chat.id, text='Укажите ваш номер телефона', reply_markup=keyboard)
+        bot.register_next_step_handler(msg, master_finish)
+        user["flag_contact"] = False
 
 # обработка нажатия клавиши МАСТЕР-КЛАСС
 @bot.callback_query_handler(func=lambda call: call.data == "masterclass")
 def callback_picture(call):
+    print("callback_picture")
+    bot.clear_step_handler(call.message)
     bot.send_message(call.message.chat.id,
-                     text='Оставьте свои контактные данные для связи',
+                     text='Расписание находится в процессе формирования. Оставьте свои контактные данные, '
+                          'чтобы записаться в лист ожидания.',
                      parse_mode="Markdown")
-    msg = bot.send_message(call.message.chat.id, text='Укажите ваши имя и фамилию')
-    bot.register_next_step_handler(msg, get_contact)
+    msg = bot.send_message(call.message.chat.id, text='Укажите ваши имя и фамилию:')
+    bot.register_next_step_handler(msg, get_contact_master)
+
+def picture_finish(message):
+    print("picture_finish")
+    googleSheets.update_phone(message)
+    bot.send_message(message.chat.id, text='Благодарим за проявленный интерес!'
+                                           ' О появлении картин вы узнаете в числе первых!')
+
+def master_finish(message):
+    print("master_finish")
+    googleSheets.update_phone_master(message)
+    bot.send_message(message.chat.id, text='Благодарим, вы записаны в лист ожидания.'
+                                           ' Мы свяжемся с вами и сообщим о дате ближайшего мастер-класса.')
 
 if __name__ == '__main__':
-    # bot.polling()
+# bot.polling()
     while True:
         try:
             bot.polling()
--- googleSheets.py
+++ googleSheets.py
@@ -6,13 +6,21 @@ gp = gspread.service_account(filename='aquarel.json')
 #Open Google spreadsheet
 gsheet = gp.open("AquarelArt")
 #Select worksheet
-wsheet = gsheet.worksheet("Запись на мастер класс")
+wsheet = gsheet.worksheet("Нейрокартины")
+masterClass = gsheet.worksheet("Мастер-класс")
+start = gsheet.worksheet("/start")
 # получить все значения
 # wsheet.get_all_values()
 
 # добавить значения
 def append_name(ID, name):
     wsheet.append_row([ID, name])
+
+def append_name_start(ID):
+    start.append_row([ID])
+
+def append_name_master(ID, name):
+    masterClass.append_row([ID, name])
 
 # поиск строки и столбца положения значения
 def values_row_col(value):
@@ -24,16 +32,51 @@ def values_row_col(value):
                 res.append({'row': i, 'col': j})
     return res
 
+def values_row_col_master(value):
+    print("values_row_col_master")
+    values = masterClass.get_all_values()
+    res = []
+    for i, r in enumerate(values):
+        for j, c in enumerate(r):
+            if str(value) in c:
+                res.append({'row': i, 'col': j})
+    return res
 # добавления значения
 def update_phone(message):
+    print("update_phone")
     ID = message.chat.id
+    print(f"ID: {ID}")
     res = values_row_col(ID)
+    print(res)
+    row = res[-1]["row"]+1
+    print(f"row: {row}")
+    if message.contact != None:
+        print("contact")
+        phone = message.contact.phone_number
+    else:
+        print("text")
+        phone = message.text
+    wsheet.update(f'C{row}', phone)
+
+def update_phone_master(message):
+    print("update_phone_master")
+    ID = message.chat.id
+    res = values_row_col_master(ID)
     row = res[-1]["row"]+1
     if message.contact != None:
         phone = message.contact.phone_number
     else:
         phone = message.text
-    wsheet.update(f'C{row}', phone)
+    masterClass.update(f'C{row}', phone)
+
+
+def get_all_user():
+    values = start.get_all_values()
+    users = set()
+    for user in values:
+        users.add(user[0])
+    print(users)
+    return list(users)
 
 if __name__ == '__main__':
     values_row_col(value='anna')
